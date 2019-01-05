4c9e39cc2d9cb36bcad8dab2059688d2c8934bff
Shakira Weir <shakira.weir87@gmail.com>
2019-01-05 04:57:21 -0500

    updated header

--- dist/main.8f75cf09.js
+++ dist/main.8f75cf09.js
@@ -141,7 +141,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58365" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64434" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
