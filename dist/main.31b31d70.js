aa0478786b2aec4a052309644418d168a57d8b69
Shakira Weir <shakira.weir87@gmail.com>
2019-01-05 20:23:34 -0500

    completed css for main content

--- dist/main.31b31d70.js
+++ dist/main.31b31d70.js
@@ -203,7 +203,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64434" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57094" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
